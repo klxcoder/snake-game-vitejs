da730d83ce9bc3b9fe2de6cfb7c3a393e44fc200
klxcoder <klxcoder@gmail.com>
2025-03-14 10:16:05 +0700

    Board

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,8 +1,11 @@
 import { RowCol } from "./RowCol";
 
 export class Board {
-  // valid row from `0` to `size.row-1`
-  // valid col from `0` to `size.col-1`
+  /**
+   * - Define the size of the board
+   * - Valid row from `0` to `size.row-1`
+   * - Valid col from `0` to `size.col-1`
+   */
   public size: RowCol
   public constructor({
     size,
