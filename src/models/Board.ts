f395405bd9020e48a27c93036a8387b5682e0bd3
klxcoder <klxcoder@gmail.com>
2025-03-14 08:30:22 +0700

    Board has size

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,3 +1,12 @@
-export class Board {
+import { RowCol } from "./RowCol";
 
+export class Board {
+  public size: RowCol
+  public constructor({
+    size,
+  }: {
+    size: RowCol,
+  }) {
+    this.size = size
+  }
 }
