7405302d36a32ea49f5cdade0fae9423105cd66e
klxcoder <klxcoder@gmail.com>
2025-03-14 11:34:36 +0700

    test isSelfEaten

--- src/models/Snake.test.ts
+++ src/models/Snake.test.ts
@@ -3,7 +3,7 @@ import { Snake } from './Snake';
 import { RowCol } from './RowCol';
 
 describe('tick', () => {
-  it('tick, goUp, doDown, turnLeft, turnRight', () => {
+  it('tick, go', () => {
     const snake: Snake = new Snake({
       body: new Array(4).fill(1).map((_, index) => {
         return new RowCol({
@@ -19,5 +19,28 @@ describe('tick', () => {
     expect(snake.getRawBody()).toStrictEqual([0, 2, 0, 3, 0, 4, 1, 4])
     snake.tick()
     expect(snake.getRawBody()).toStrictEqual([0, 3, 0, 4, 1, 4, 2, 4])
+  });
+  it('isSelfEaten', () => {
+    const snake: Snake = new Snake({
+      body: new Array(4).fill(1).map((_, index) => {
+        return new RowCol({
+          row: 0,
+          col: index,
+        })
+      }),
+    })
+    expect(snake.getRawBody()).toStrictEqual([0, 0, 0, 1, 0, 2, 0, 3])
+    expect(snake.isSelfEaten(new RowCol({
+      row: 0,
+      col: 0,
+    }))).toStrictEqual(true)
+    expect(snake.isSelfEaten(new RowCol({
+      row: 0,
+      col: 3,
+    }))).toStrictEqual(true)
+    expect(snake.isSelfEaten(new RowCol({
+      row: 0,
+      col: 4,
+    }))).toStrictEqual(false)
   });
 });
