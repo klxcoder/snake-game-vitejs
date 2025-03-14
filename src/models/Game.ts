33b399094c055eee074cd9c2917509e0315f8d37
klxcoder <klxcoder@gmail.com>
2025-03-14 08:35:32 +0700

    generateFood

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,9 +1,18 @@
 import { Board } from "./Board";
+import { RowCol } from "./RowCol";
 import { Snake } from "./Snake";
 
 export class Game {
   public snake: Snake
   public board: Board
+  public food: RowCol
+  public generateFood(): RowCol {
+    const food: RowCol = {
+      row: 5,
+      col: 5,
+    }
+    return food
+  }
   public constructor({
     snake,
     board,
@@ -13,5 +22,6 @@ export class Game {
   }) {
     this.snake = snake
     this.board = board
+    this.food = this.generateFood()
   }
 }
