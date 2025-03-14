5043b54ea76ed44c0b23bb087c9a624f52a3166b
klxcoder <klxcoder@gmail.com>
2025-03-14 08:26:36 +0700

    Game constructor

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,3 +1,17 @@
-export class Game {
+import { Board } from "./Board";
+import { Snake } from "./Snake";
 
+export class Game {
+  public snake: Snake
+  public board: Board
+  public constructor({
+    snake,
+    board,
+  }: {
+    snake: Snake,
+    board: Board,
+  }) {
+    this.snake = snake
+    this.board = board
+  }
 }
