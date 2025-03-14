37aacd04a92ceb1ff8559aaa9e175c0b133bca1f
klxcoder <klxcoder@gmail.com>
2025-03-14 12:24:10 +0700

    getRandomHSLColor

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,8 @@
 export function getRandomInt(a: number, b: number) {
   return Math.floor(Math.random() * (b - a + 1)) + a;
 }
+
+export function getRandomHSLColor() {
+  const hue = Math.floor(Math.random() * 360); // 0-359
+  return `hsl(${hue}, 100%, 50%)`;
+}
