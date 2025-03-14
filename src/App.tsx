4c5931aadf426eff996dcaea5a337b11fbf6a3a5
klxcoder <klxcoder@gmail.com>
2025-03-14 12:12:07 +0700

    const game = useMemo(() => {

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { useRef } from 'react'
+import { useMemo, useRef } from 'react'
 import styles from './App.module.scss'
+import { Game } from './models/Game'
+import { Snake } from './models/Snake'
+import { Board } from './models/Board'
+import { RowCol } from './models/RowCol'
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  const game = useMemo(() => {
+    const snake = new Snake({
+      body: new Array(4).fill(1).map((_, index) => (
+        new RowCol({ row: 0, col: index })
+      )),
+    })
+    const board = new Board({
+      size: new RowCol({ row: 15, col: 20 })
+    })
+    return new Game({ snake, board })
+  }, [])
 
   return (
     <div className={styles.app}>
       <canvas
         className={styles.canvas}
+        style={{
+          width: game.board.size.col * 30,
+          height: game.board.size.row * 30,
+        }}
         ref={canvasRef}
       >
       </canvas>
