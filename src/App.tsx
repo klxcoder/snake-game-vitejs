4fd84e6ca1e688f9b682a51914d6f260df74844d
klxcoder <klxcoder@gmail.com>
2025-03-14 12:03:28 +0700

    empty canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useRef } from 'react'
 import styles from './App.module.scss'
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   return (
     <div className={styles.app}>
-      Minimal
+      <canvas
+        className={styles.canvas}
+        ref={canvasRef}
+      >
+      </canvas>
     </div>
   )
 }
